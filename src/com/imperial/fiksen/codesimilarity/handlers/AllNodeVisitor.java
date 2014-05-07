9843baf7e7d6180fb730a9801a5e18e6d5cb75ca
Adam Fiksen <fiksen99@gmail.com>
2014-05-07 11:11:05 +0100

    create the visitor for AST tree

--- src/com/imperial/fiksen/codesimilarity/handlers/AllNodeVisitor.java
+++ src/com/imperial/fiksen/codesimilarity/handlers/AllNodeVisitor.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 
-public class AllVisitor extends ASTVisitor {
+public class AllNodeVisitor extends ASTVisitor {
 	
 	Stack<ASTNodeWithChildren> currentRootStack = new Stack<ASTNodeWithChildren>();
 	ASTNodeWithChildren root;
@@ -13,8 +13,8 @@ public class AllVisitor extends ASTVisitor {
 	public void preVisit(ASTNode node) {
 		ASTNodeWithChildren nodeWC = new ASTNodeWithChildren(node);
 		if (!currentRootStack.isEmpty()) {
-			ASTNodeWithChildren root = currentRootStack.peek();
-			root.addChild(nodeWC);
+			ASTNodeWithChildren parent = currentRootStack.peek();
+			parent.addChild(nodeWC);
 		} else {
 			root = nodeWC;
 		}
