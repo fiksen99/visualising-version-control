7399fd164d266362642afe7406af52993b0970c5
Adam Fiksen <fiksen99@gmail.com>
2014-05-19 17:21:57 +0100

    no need to recompute if wanting a different print format

--- src/com/imperial/fiksen/codesimilarity/handlers/GetInfo.java
+++ src/com/imperial/fiksen/codesimilarity/handlers/GetInfo.java
@@ -1,5 +1,8 @@
 package com.imperial.fiksen.codesimilarity.handlers;
 
+import java.util.List;
+import java.util.Set;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -10,8 +13,11 @@ import org.eclipse.core.resources.ResourcesPlugin;
 
 import com.imperial.fiksen.codesimilarity.analysers.SimilarityAnalyser;
 import com.imperial.fiksen.codesimilarity.parseTreeKernel.ParseTreeKernelSimilarityAnalyser;
+import com.imperial.fiksen.codesimilarity.utils.ResultsPrinter;
 
 public class GetInfo extends AbstractHandler {
+	
+	SimilarityAnalyser analyser = new ParseTreeKernelSimilarityAnalyser();
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -19,8 +25,14 @@ public class GetInfo extends AbstractHandler {
 		IWorkspaceRoot root = workspace.getRoot();
 		IProject[] projects = root.getProjects();
 		//TODO: dependency injection of analyser
-		SimilarityAnalyser analyser = new ParseTreeKernelSimilarityAnalyser();
-		analyser.analyse(projects);
+		if(analyser.getScores()==null) {
+			analyser.analyse(projects);
+		} else {
+			double[][] scores = analyser.getScores();
+			List<String> orderedProjects = analyser.getOrderedProjects();
+			Set<Integer> toIgnore = analyser.getToIgnore();
+			ResultsPrinter.print(scores, orderedProjects, toIgnore);
+		}
 		return null;
 	}
 
