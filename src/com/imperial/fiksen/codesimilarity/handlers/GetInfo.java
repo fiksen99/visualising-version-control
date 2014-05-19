92a832cb0ae6e8c1a5fe1c9cbb2d71c36ab12913
Adam Fiksen <fiksen99@gmail.com>
2014-05-19 17:34:26 +0100

    remove GetInfo analysis from main thread

--- src/com/imperial/fiksen/codesimilarity/handlers/GetInfo.java
+++ src/com/imperial/fiksen/codesimilarity/handlers/GetInfo.java
@@ -10,6 +10,10 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 
 import com.imperial.fiksen.codesimilarity.analysers.SimilarityAnalyser;
 import com.imperial.fiksen.codesimilarity.parseTreeKernel.ParseTreeKernelSimilarityAnalyser;
@@ -21,18 +25,27 @@ public class GetInfo extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IWorkspaceRoot root = workspace.getRoot();
-		IProject[] projects = root.getProjects();
-		//TODO: dependency injection of analyser
-		if(analyser.getScores()==null) {
-			analyser.analyse(projects);
-		} else {
-			double[][] scores = analyser.getScores();
-			List<String> orderedProjects = analyser.getOrderedProjects();
-			Set<Integer> toIgnore = analyser.getToIgnore();
-			ResultsPrinter.print(scores, orderedProjects, toIgnore);
-		}
+		Job job = new Job("analyseAll") {
+			  @Override
+			  protected IStatus run(IProgressMonitor monitor) {
+					IWorkspace workspace = ResourcesPlugin.getWorkspace();
+					IWorkspaceRoot root = workspace.getRoot();
+					IProject[] projects = root.getProjects();
+					//TODO: dependency injection of analyser
+					if(analyser.getScores()==null) {
+						analyser.analyse(projects);
+					} else {
+						double[][] scores = analyser.getScores();
+						List<String> orderedProjects = analyser.getOrderedProjects();
+						Set<Integer> toIgnore = analyser.getToIgnore();
+						ResultsPrinter.print(scores, orderedProjects, toIgnore);
+					}
+					return Status.OK_STATUS;
+			  }
+				  
+			};
+
+		job.schedule(); 
 		return null;
 	}
 
