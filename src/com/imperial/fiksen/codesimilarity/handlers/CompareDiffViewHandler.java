9c3873514d3b211c6b0ec50b5eb6945090dc25ac
Adam Fiksen <fiksen99@gmail.com>
2014-06-04 22:43:58 +0100

    Implement Compare view for 2 files
    a lot of the code comes from and MANY thanks go to the egit team, in
    particular the classes
    org.eclipse.egit.ui.internal.actions.RepositoryActionHandler and
    org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput

---
+++ src/com/imperial/fiksen/codesimilarity/handlers/CompareDiffViewHandler.java
@@ -0,0 +1,71 @@
+package com.imperial.fiksen.codesimilarity.handlers;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.CompareEditorInput;
+import org.eclipse.compare.CompareUI;
+import org.eclipse.compare.ITypedElement;
+import org.eclipse.compare.structuremergeviewer.ICompareInput;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.team.ui.synchronize.SaveableCompareEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+import com.imperial.fiksen.codesimilarity.compare.SimilarityCompareEditorInput;
+
+public class CompareDiffViewHandler extends AbstractHandler {
+	private static final int NUM_COMPARABLE_FILES = 2;
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IFile left = null;
+		IFile right = null;
+		IWorkbenchPage page = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage();
+	    ISelection selection = page.getSelection();
+        if (selection != null & selection instanceof IStructuredSelection) {
+          IStructuredSelection strucSelection = (IStructuredSelection) selection;
+          Iterator<Object> iterator = strucSelection.iterator();
+          int selectedFiles = 0;
+          while(iterator.hasNext()) {
+            Object element = iterator.next();
+            //if the selected item is part of a java project
+            if(element instanceof ICompilationUnit) {
+            	
+            	ICompilationUnit i = (ICompilationUnit) element;
+            	IFile file = (IFile) i.getResource();
+            	//i.get
+            	if( left == null ) {
+            		left = file;
+            	} else {
+            		right = file;
+            	}
+            	selectedFiles++;
+            }
+          }
+          if(selectedFiles != NUM_COMPARABLE_FILES ) {
+        	  throw new ExecutionException("Must only select " + NUM_COMPARABLE_FILES + " files");
+          }
+        }		
+
+		ITypedElement leftTypedElem = SaveableCompareEditorInput.createFileElement(left);
+		ITypedElement rightTypedElem = SaveableCompareEditorInput.createFileElement(right);
+		
+		CompareEditorInput in = new SimilarityCompareEditorInput(leftTypedElem, rightTypedElem, page);
+		CompareUI.openCompareEditor(in);
+		return null;
+	}
+}
